91607be3053ee33dc5259c86bcf5ea7738c956cd
Adam <packetdev@gmail.com>
2016-08-06 19:21:36 -0400

    Adds Nameplates module. Probably doesn't work, #YOLO

--- src/main/java/com/ulfric/core/Core.java
+++ src/main/java/com/ulfric/core/Core.java
@@ -1,6 +1,7 @@
 package com.ulfric.core;
 
 import com.ulfric.core.economy.ModuleEconomyInterface;
+import com.ulfric.core.modules.ModuleNameplates;
 import com.ulfric.core.modules.ModuleTrash;
 import com.ulfric.core.modules.ModuleWelcome;
 import com.ulfric.core.scoreboard.ModuleScoreboardImplementation;
@@ -14,6 +15,7 @@ public class Core extends Plugin {
 		this.addModule(new ModuleEconomyInterface());
 		this.addModule(new ModuleWelcome());
 		this.addModule(new ModuleScoreboardImplementation());
+		this.addModule(new ModuleNameplates());
 		this.addModule(new ModuleTrash());
 		//this.addModule(new ModuleBackpack()); // TODO: Finish this module
 	}
---
+++ src/main/java/com/ulfric/core/modules/ModuleNameplates.java
@@ -0,0 +1,325 @@
+package com.ulfric.core.modules;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import com.google.common.collect.Maps;
+import com.ulfric.config.ConfigFile;
+import com.ulfric.config.MutableDocument;
+import com.ulfric.data.DataAddress;
+import com.ulfric.data.MultiSubscription;
+import com.ulfric.data.scope.PlayerScopes;
+import com.ulfric.lib.coffee.command.Command;
+import com.ulfric.lib.coffee.data.DataManager;
+import com.ulfric.lib.coffee.event.Handler;
+import com.ulfric.lib.coffee.event.Listener;
+import com.ulfric.lib.coffee.module.Module;
+import com.ulfric.lib.coffee.numbers.NumberUtils;
+import com.ulfric.lib.coffee.string.NamedBase;
+import com.ulfric.lib.craft.entity.player.Player;
+import com.ulfric.lib.craft.entity.player.PlayerUtils;
+import com.ulfric.lib.craft.event.player.PlayerJoinEvent;
+import com.ulfric.lib.craft.inventory.item.ItemStack;
+import com.ulfric.lib.craft.inventory.item.ItemUtils;
+import com.ulfric.lib.craft.inventory.item.Material;
+import com.ulfric.lib.craft.panel.Button;
+import com.ulfric.lib.craft.panel.Panel;
+import com.ulfric.lib.craft.panel.standard.StandardPanel;
+import com.ulfric.lib.craft.scoreboard.Scoreboard;
+import com.ulfric.lib.craft.scoreboard.ScoreboardTeam;
+import com.ulfric.lib.craft.string.ChatUtils;
+
+public class ModuleNameplates extends Module {
+
+	public ModuleNameplates()
+	{
+		super("nameplates", "Module for coloring your nameplate", "1.0.0", "Packet");
+	}
+
+	Map<String, Nameplate> nameplates;
+	Map<UUID, NameplateKey> playerNameplates;
+	private MultiSubscription<UUID, String> subscription;
+	BiConsumer<Player, Nameplate> consumer;
+
+	@Override
+	public void onModuleEnable()
+	{
+		if (!this.subscription.isSusbcribed())
+		{
+			this.subscription.subscribe();
+		}
+
+		ConfigFile config = this.getModuleConfig();
+		MutableDocument document = config.getRoot();
+
+		MutableDocument nameplateDocument = document.getDocument("nameplates");
+
+		if (nameplateDocument == null)
+		{
+			nameplateDocument = document.createDocument("nameplates");
+
+			MutableDocument exampleDocument = nameplateDocument.createDocument("example");
+
+			exampleDocument.set("item", Material.of("GRASS").toItem());
+			exampleDocument.set("prefix", "[EXAMPLE] ");
+
+			config.save();
+		}
+
+		for (String key : nameplateDocument.getKeys(false))
+		{
+			MutableDocument nameplateParse = nameplateDocument.getDocument(key);
+
+			ItemStack item = ItemUtils.getItem(nameplateParse.getString("item"));
+			String prefix = ChatUtils.color(nameplateParse.getString("prefix"));
+
+			Nameplate nameplate = new Nameplate(key, item, prefix);
+
+			this.nameplates.put(key, nameplate);
+		}
+	}
+
+	@Override
+	public void onModuleDisable()
+	{
+		this.subscription.unsubscribe();
+
+		int count = 0;
+
+		for (Entry<UUID, NameplateKey> entry : this.playerNameplates.entrySet())
+		{
+			NameplateKey value = entry.getValue();
+
+			if (value.isNotModified()) continue;
+
+			count++;
+
+			this.subscription.get(entry.getKey()).setValue(value.getKey());
+		}
+
+		if (count > 0)
+		{
+			if (count == 1)
+			{
+				this.log("Wrote 1 nameplate change to the database.");
+			}
+			else
+			{
+				this.log("Wrote " + count + " nameplate changes to the database.");
+			}
+		}
+
+		this.nameplates.clear();
+		this.playerNameplates.clear();
+	}
+
+	private class Nameplate extends NamedBase
+	{
+		Nameplate(String name, ItemStack item, String prefix)
+		{
+			super(name);
+
+			this.item = item;
+			this.prefix = prefix;
+		}
+
+		private final ItemStack item;
+		private final String prefix;
+
+		public ItemStack getItem()
+		{
+			return this.item;
+		}
+
+		public String getPrefix()
+		{
+			return this.prefix;
+		}
+	}
+
+	private class NameplateKey
+	{
+		NameplateKey(String key, boolean modified)
+		{
+			this.key = key;
+
+			this.modified = modified;
+		}
+
+		private String key;
+		private boolean modified;
+
+		public String getKey()
+		{
+			return this.key;
+		}
+
+		public void setKey(String key)
+		{
+			this.key = key;
+
+			this.modified = true;
+		}
+
+		public boolean isModified()
+		{
+			return this.modified;
+		}
+
+		public boolean isNotModified()
+		{
+			return !this.isModified();
+		}
+	}
+
+	@Override
+	public void onFirstEnable()
+	{
+		this.nameplates = Maps.newHashMap();
+		this.playerNameplates = Maps.newHashMap();
+
+		this.subscription = DataManager.get()
+									   .getDatabase("nameplates")
+						 			   .multi(String.class, PlayerScopes.ONLINE, new DataAddress<>("nameplates", null, null))
+						 			   .onChange((oldValue, newValue) ->
+						 			   {
+						 				   this.playerNameplates.put(newValue.getAddress().getId(), new NameplateKey(newValue.getValue(), false));
+						 			   })
+						 			   .blockOnSubscribe(true)
+						 			   .subscribe();
+
+		Executor executor = Executors.newSingleThreadExecutor();
+
+		this.consumer = (player, plate) ->
+		{
+			String prefix = plate.getPrefix();
+			String plateName = plate.getName();
+			String name = player.getName();
+
+			for (Player allPlayers : PlayerUtils.getOnlinePlayers())
+			{
+				if (allPlayers == player) continue;
+
+				Scoreboard scoreboard = allPlayers.scoreboard();
+
+				ScoreboardTeam team = scoreboard.getTeam(plateName);
+
+				if (team == null)
+				{
+					team = scoreboard.createTeam(plateName);
+
+					team.setPrefix(prefix);
+				}
+
+				team.addEntry(name);
+			}
+		};
+
+		Consumer<Player> youConsumer = player ->
+		{
+			Scoreboard scoreboard = player.scoreboard();
+
+			ScoreboardTeam self = scoreboard.getOrCreateTeam("_self");
+			self.setPrefix(player.getLocalizedMessage("nameplate.self"));
+			self.addEntry(player.getName());
+		};
+
+		this.addListener(new Listener(this)
+		{
+			@Handler
+			public void onJoin(PlayerJoinEvent event)
+			{
+				Player player = event.getPlayer();
+
+				executor.execute(() -> youConsumer.accept(player));
+
+				NameplateKey key = ModuleNameplates.this.playerNameplates.get(player.getUniqueId());
+
+				if (key == null) return;
+
+				Nameplate nameplate = ModuleNameplates.this.nameplates.get(key.getKey());
+
+				if (nameplate == null) return;
+
+				ModuleNameplates.this.consumer.accept(player, nameplate);
+			}
+		});
+
+		this.addCommand(new CommandNameplates());
+	}
+
+	private class CommandNameplates extends Command
+	{
+		public CommandNameplates()
+		{
+			super("nameplates", ModuleNameplates.this, "nameplate");
+
+			this.addEnforcer(Player.class::isInstance, "nameplate.must_be_player");
+		}
+
+		@Override
+		public void run()
+		{
+			Player player = (Player) this.getSender();
+
+			Map<Integer, Entry<String, Nameplate>> availableNameplates = Maps.newHashMap();
+
+			int slot = 0;
+
+			for (Entry<String, Nameplate> entry : ModuleNameplates.this.nameplates.entrySet())
+			{
+				if (!player.hasPermission("nameplates." + entry.getKey())) continue;
+
+				availableNameplates.put(slot++, entry);
+			}
+
+			if (availableNameplates.isEmpty())
+			{
+				player.sendLocalizedMessage("nameplate.none_available");
+
+				return;
+			}
+
+			StandardPanel panel = Panel.createStandard(NumberUtils.roundUp(availableNameplates.size(), 9), player.getLocalizedMessage("nameplate.panel"));
+
+			slot = 0;
+
+			for (Entry<Integer, Entry<String, Nameplate>> nameplate : availableNameplates.entrySet())
+			{
+				Entry<String, Nameplate> plateWrapper = nameplate.getValue();
+
+				panel.addButton(Button.builder().addSlot(slot++, plateWrapper.getValue().getItem()).addAction(event ->
+				{
+					Entry<String, Nameplate> entry = availableNameplates.get(event.getSlotObject());
+
+					if (entry == null) return;
+
+					Player clicker = event.getPlayer();
+
+					NameplateKey key = ModuleNameplates.this.playerNameplates.get(clicker.getUniqueId());
+
+					if (key == null)
+					{
+						key = new NameplateKey(entry.getKey(), true);
+					}
+					else
+					{
+						key.setKey(entry.getKey());
+					}
+
+					clicker.inv().close();
+
+					ModuleNameplates.this.consumer.accept(clicker, entry.getValue());
+				}).build());
+			}
+
+			panel.open(player, false);
+		}
+	}
+
+}
