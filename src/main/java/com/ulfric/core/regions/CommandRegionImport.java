2a12c8784b94b394ed01ff2414ac623afa3a4b2e
Adam <packetdev@gmail.com>
2016-08-18 07:25:09 -0400

    /region import

---
+++ src/main/java/com/ulfric/core/regions/CommandRegionImport.java
@@ -0,0 +1,31 @@
+package com.ulfric.core.regions;
+
+import com.ulfric.config.ConfigFile;
+import com.ulfric.lib.coffee.command.Argument;
+import com.ulfric.lib.coffee.command.Command;
+import com.ulfric.lib.coffee.module.ModuleBase;
+import com.ulfric.lib.coffee.region.Region;
+import com.ulfric.lib.craft.region.RegionColl;
+
+final class CommandRegionImport extends Command {
+
+	public CommandRegionImport(ModuleBase owner)
+	{
+		super("import", owner);
+
+		this.addArgument(Argument.builder().setPath("file").addSimpleResolver(owner::getModuleConfig).build());
+	}
+
+	@Override
+	public void run()
+	{
+		ConfigFile file = (ConfigFile) this.getObject("file");
+
+		Region region = Region.fromDocument(file.getRoot());
+
+		if (region == null) return;
+
+		RegionColl.registerRegion(region);
+	}
+
+}
--- src/main/java/com/ulfric/core/regions/CommandRegions.java
+++ src/main/java/com/ulfric/core/regions/CommandRegions.java
@@ -1,6 +1,7 @@
 package com.ulfric.core.regions;
 
 import com.ulfric.lib.coffee.command.Command;
+import com.ulfric.lib.coffee.command.CommandKey;
 import com.ulfric.lib.coffee.module.ModuleBase;
 
 final class CommandRegions extends Command {
@@ -8,6 +9,9 @@ final class CommandRegions extends Command {
 	public CommandRegions(ModuleBase owner)
 	{
 		super("regions", owner, "region", "rg", "guards", "guard", "gu");
+
+		Command command = new CommandRegionImport(owner);
+		this.addCommand(command, CommandKey.singular(command.getName()));
 
 		this.addPermission("regions.use");
 	}
