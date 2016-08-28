5c046debcd2449bde475e06b2787fe66130effe1
Adam <packetdev@gmail.com>
2016-08-28 06:41:48 -0400

    support times variable in multi potion rewards

--- src/main/java/com/ulfric/core/reward/MultiPotionReward.java
+++ src/main/java/com/ulfric/core/reward/MultiPotionReward.java
@@ -1,11 +1,14 @@
 package com.ulfric.core.reward;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
 
+import com.ulfric.lib.coffee.collection.MapUtils;
 import com.ulfric.lib.craft.entity.player.Player;
 import com.ulfric.lib.craft.potion.PotionEffect;
+import com.ulfric.lib.craft.potion.PotionUtils;
 
 final class MultiPotionReward implements Reward {
 
@@ -31,6 +34,27 @@ final class MultiPotionReward implements Reward {
 	@Override
 	public void give(Player player, String reason, Object... objects)
 	{
+		if (objects.length > 0)
+		{
+			Map<String, Object> map = MapUtils.newHashMap(objects);
+
+			Object rawGet = map.get("times");
+
+			if (rawGet instanceof Integer)
+			{
+				int get = (int) rawGet;
+
+				for (PotionEffect o : this.effects)
+				{
+					PotionEffect clonedEffect = PotionUtils.newEffect(o.getType(), o.getDuration() * get, o.getAmplifier(), o.isAmbient(), o.hasParticles());
+
+					clonedEffect.apply(player);
+				}
+
+				return;
+			}
+		}
+
 		for (PotionEffect effect : this.effects)
 		{
 			effect.apply(player);
