d39fdccefb96a6e21c9b147f417bd670ff6ce045
Adam <packetdev@gmail.com>
2016-08-29 03:51:59 -0400

    Make omnitool ensure the tool is made of diamond

--- src/main/java/com/ulfric/core/enchant/EnchantmentOmnitool.java
+++ src/main/java/com/ulfric/core/enchant/EnchantmentOmnitool.java
@@ -1,6 +1,7 @@
 package com.ulfric.core.enchant;
 
 import com.google.common.collect.ImmutableList;
+import com.ulfric.lib.craft.inventory.item.ItemStack;
 import com.ulfric.lib.craft.inventory.item.enchant.Enchantment;
 
 public final class EnchantmentOmnitool extends Enchantment {
@@ -10,6 +11,14 @@ public final class EnchantmentOmnitool extends Enchantment {
 	private EnchantmentOmnitool()
 	{
 		super("Omnitool", 242, 1, ImmutableList.of());
+	}
+
+	@Override
+	public boolean canEnchant(ItemStack item)
+	{
+		if (!super.canEnchant(item)) return false;
+
+		return item.getType().getName().startsWith("DIAMOND_");
 	}
 
 }
