07fbb4bd9bdef25b93c54cc6a17c855def9d8328
Adam <packetdev@gmail.com>
2016-09-07 07:02:43 -0400

    if it's not on the javadocs, it's #YOLO

--- src/main/java/com/ulfric/core/modules/ModuleCandy.java
+++ src/main/java/com/ulfric/core/modules/ModuleCandy.java
@@ -86,7 +86,7 @@ public class ModuleCandy extends Module {
 	}
 
 	private final List<String> strings = Arrays.asList("munch", "crunch");
-	private final PlayableSound sound = PlayableSound.builder().setSound("ENTITY_PLAYER_BURP").setVolume(5).setPitch(4).build();
+	private final PlayableSound sound = PlayableSound.builder().setSound("EAT").setVolume(1).setPitch(1).build();
 	private Candy fromDocument(Document document)
 	{
 		MaterialData material = MaterialData.of(document.getString("material"));
